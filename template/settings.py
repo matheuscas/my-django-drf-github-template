e57f6a183c9944099020be1cc503826b578c86bb
matheuscas <matheus.mcas@gmail.com>
2024-01-29 21:23:13 -0300

    fix: add rest_framework to installed apps

--- template/settings.py
+++ template/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     # Third party
+    "rest_framework",
     "drf_spectacular",
 ]
 
--- template/urls.py
+++ template/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView,
                                    SpectacularSwaggerView)
 
@@ -24,6 +24,7 @@ urlpatterns = [
 ]
 
 urlpatterns += [
+    path('api-auth/', include('rest_framework.urls')),
     # YOUR PATTERNS
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     # Optional UI:
